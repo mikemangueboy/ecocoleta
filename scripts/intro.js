402fde52a999a0af6a90522063f90a354d1b28e7
Mayck <mayck@MacBook-Pro-de-Mayck.local>
2020-06-02 14:37:44 -0300

    aula 2 com js

---
+++ scripts/intro.js
@@ -0,0 +1,70 @@
+ //document.write ("Hello")
+
+//var myvar = "hello"
+//const myconst = "dale"
+
+//document.write(myconst + myvar)  
+
+// string
+//const s1 = "Isso é uma string"
+//const s2 = 'isso também é um string'
+//const s3 = `isso é uma string também`
+//document.write(s1)
+
+//number
+
+//const n1 = 1
+//const n2 = 12
+//document.write(n1 + n2)
+
+
+//bolean - true or false
+//const bTrue = true
+//cosnt bFalde = true
+//document.write(bFalse)
+
+// objeto
+
+//  const pessoa = {
+//    altura: "1,80m",
+//    idade: 24,
+//    solteiro: true,
+//    correr(){
+//      return "run Forest"
+//    }
+//  }
+ 
+//   pessoa.correr())
+
+//Array - Vetores
+
+// const colecaodebolinhas = ["blue", "green", 1, {name: "My name"}]
+
+// document.write(colecaodebolinhas[3].name )
+
+// Funcoes - Funcionalidades - Atalhos - Reuso de códigos
+
+// function sayMyName(name) {
+//   document.write (name)
+// }
+
+// sayMyName("jesus")
+// sayMyName("pedro")
+// sayMyName("joao")
+// sayMyName("godi")
+
+// condicionais
+
+// const notaFinal = 3
+
+// if (notaFinal < 5) {
+//   document.write ("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+//loop repetições
+
+for (i = 0; i < 10; i++) {
+    document.write(`<p>${i}</p>`)
+}
